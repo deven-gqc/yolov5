8ed54d75cc2cf5b6351436e7547987629438afd2
deven-gqc <deven@gqc.com>
2021-07-09 10:46:29 -0400

    add code for saving images from a corner

--- utils/plots.py
+++ utils/plots.py
@@ -67,18 +67,31 @@ def butter_lowpass_filtfilt(data, cutoff=1500, fs=50000, order=5):
     return filtfilt(b, a, data)  # forward-backward filter
 
 
+c = 0
+
+
 def plot_one_box(x, im, color=(128, 128, 128), label=None, line_thickness=3):
+    global c
     # Plots one bounding box on image 'im' using OpenCV
     assert im.data.contiguous, 'Image not contiguous. Apply np.ascontiguousarray(im) to plot_on_box() input image.'
     tl = line_thickness or round(0.002 * (im.shape[0] + im.shape[1]) / 2) + 1  # line/font thickness
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
-    cv2.rectangle(im, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    c3 = c1 + c2 # to get cordinates of both points in a single tuple
+    width, height, _ = im.shape
+    n = label.split()[0] # get name of the vehicle, car truck etc.
+    if n == 'car' and c1[0] <= height//2 and c1[1] >= width//2 and \
+            c2[0] <= height//2 and c2[1] >= width//2:
+        cv2.rectangle(im, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+        im2 = Image.fromarray(im)
+        crop = im2.crop(c3)
+        c += 1
+        crop.save('/content/snips/' + str(c) + '.jpg')
     if label:
         tf = max(tl - 1, 1)  # font thickness
         t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
         c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
-        cv2.rectangle(im, c1, c2, color, -1, cv2.LINE_AA)  # filled
-        cv2.putText(im, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+        # cv2.rectangle(im, c1, c2, color, -1, cv2.LINE_AA)  # filled
+        # cv2.putText(im, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
 
 
 def plot_one_box_PIL(box, im, color=(128, 128, 128), label=None, line_thickness=None):
